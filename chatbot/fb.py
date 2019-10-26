35915419a356cf37d102d611087b49b72d79c904
Matt Katz <mykatz01@gmail.com>
2019-10-26 03:57:07 -0700

    integration w/ fb

---
+++ chatbot/fb.py
@@ -0,0 +1,55 @@
+'''
+flask api to deal with facebook stuff
+'''
+
+
+import random
+from flask import Flask, request
+from pymessenger.bot import Bot
+
+from config import *
+from dialogflow import *
+
+app = Flask(__name__)
+ACCESS_TOKEN = FB_ACCESS_TOKEN
+VERIFY_TOKEN = FB_VERIFY_TOKEN
+bot = Bot(ACCESS_TOKEN)
+
+@app.route("/", methods=['GET', 'POST'])
+def receive_message():
+    if request.method == 'GET':
+        """Before allowing people to message your bot, Facebook has implemented a verify token
+        that confirms all requests that your bot receives came from Facebook.""" 
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
+    #if request was not get, we can assume it was POST
+    else:
+        print("got a post")
+        output = request.get_json()
+        for event in output['entry']:
+          messaging = event['messaging']
+          for message in messaging:
+            if message.get('message'):
+                recipient_id = message['sender']['id'] #Facebook Messenger ID of user
+                received_msg = message['message'].get('text')
+                msgs = handle_user_message(received_msg, recipient_id)
+                if msgs:
+                    for msg in msgs:
+                        send_message(recipient_id, msg)
+    return "msg processed"
+
+def verify_fb_token(token_sent):
+    #take token sent by facebook and verify it matches the verify token you sent
+    #if they match, allow the request, else return an error 
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return 'Invalid verification token'
+
+#uses PyMessenger to send response to user
+def send_message(recipient_id, response):
+    #sends user the text message provided via input response parameter
+    bot.send_text_message(recipient_id, response)
+    return "success"
+
+if __name__ == "__main__":
+    app.run()
